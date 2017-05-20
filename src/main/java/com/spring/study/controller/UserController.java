724a05dc55af7e1ae413e6419e05e6f2e770ec25
YLS_TAB <1355508858@qq.com>
2017-05-21 00:19:34 +0800

    自定义了登录页面，并且将只能访问webapp目录下的login.jsp改成了可以访问WEB-INF/views目录下面的login.jsp的放问方式

---
+++ src/main/java/com/spring/study/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.spring.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@RequestMapping(method = RequestMethod.GET, value="/login")
+	public String login(){
+		return "login";
+	}
+
+}
